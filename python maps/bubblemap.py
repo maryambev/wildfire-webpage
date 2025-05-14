4187d54137c6ac626e79d3e58284de7235c9289a
Maryam Rafiah Beverly <148087984+maryambev@users.noreply.github.com>
2025-05-13 21:57:24 -0500

    Update bubblemap.py

--- python maps/bubblemap.py
+++ python maps/bubblemap.py
@@ -6,35 +6,6 @@ import plotly.graph_objects as go
 df = pd.read_csv("NASA_data.csv")
 df.head()
 
-
-# Q1 = df['frp'].quantile(0.1)
-# Q3 = df['frp'].quantile(1)
-# IQR = Q3 - Q1
-# lower_bound = Q1 - 1.5 * IQR
-# upper_bound = Q3 + 1.5 * IQR
-
-# df = df[(df['frp'] >= lower_bound) & (df['frp'] <= upper_bound)]
-
-# num_bins = 3  # You can adjust the number of bins as needed
-
-# # Calculate bin edges using the 'numpy.histogram_bin_edges' function
-# bin_edges = pd.qcut(df['frp'], q=num_bins, duplicates='drop', retbins=True)[1]
-
-# # First bin's lower edge and second bin's upper edge:
-# l1_lower_edge = int(bin_edges[0])
-# l1_upper_edge = int(bin_edges[1])
-
-# # Second bin's lower edge and third bin's upper edge:
-# l2_lower_edge = int(bin_edges[1])
-# l2_upper_edge = int(bin_edges[2])
-
-# # Third bin's lower edge and last bin's upper edge:
-# l3_lower_edge = int(bin_edges[2])
-# l3_upper_edge = int(bin_edges[-1])
-
-# bin_edges now contains the edges of the bins
-#print(bin_edges)
-
 # the Colored Key: numerical range by Class (0,1,2), Hover_name?
 df['text'] = 'Fire Radiative Power (FRP): ' + (df['frp']).astype(str)+' watts'
 #i want the colors to be organized into the type of wildfire (least severe 0,1, or 2 most severe)
