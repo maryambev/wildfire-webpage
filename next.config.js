598a07c7a19016d08b2e839c88f10a7e3e88ba16
maryambev <MaryamRafiah.Beverly20@kzoo.edu>
2025-05-14 08:45:37 -0500

    Configure Next.js for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    output: 'export',
+    distDir: 'out',
+    basePath: '/wildfire-webpage',
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
