ac245a87ba7d85d98d4fb2d6928d72dc5cbef5e8
maryambev <MaryamRafiah.Beverly20@kzoo.edu>
2025-05-19 14:05:04 -0500

    Update map page and config, add MapClient

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,50 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
+import dynamic from 'next/dynamic';
+
+const MapClient = dynamic(() => import('./MapClient'), { ssr: false });
 
 export const metadata = {
   title: 'Map',
   description: 'Page description',
-}
+};
 
-import Link from 'next/link'
-
-
-export default function Map() {
-  return (
-    <section className="relative">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
-        <div className="max-w-3xl mx-auto text-center pb-12 md:pb-32"></div>
-
-        <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
-          <div className="md:pl-4 lg:pl-12 xl:pl-16">
-            <div className="font-architects-daughter text-xl text-purple-600 mb-2">Ambee</div>
-            <h3 className="h3 mb-3">How to use the map</h3>
-            <p className="mb-3 text-xl text-gray-400 mb-4">Ambee's extensive historical fire data covers for the entire North American Region.
-              <div></div>
-              <br></br>1. To find your desired location, simply click and drag on the map. Scroll down to zoom out for a broader view and scroll up to zoom in for a closer look.
-              <br></br>2. Hover your cursor over a fire emoticon to view summarized information.
-              <br></br>3. Clicking on a fire emoticon will open a popup with comprehensive details, including temperature, humidity, wind speed, and other relevant factors contributing to the wildfire.
-              <br></br>4. By default, the map displays data for the last 7 days. To explore a broader timeframe, extend the date range to cover the last 30 days.
-              <br></br>5. Try finding out significant factors for wildfires?
-            </p>
-          </div>
-        </div>
-
-        <Container>
-          <div >
-            <iframe
-              src="https://wildfires.getambee.com/?w=480&h=320"
-              width='1100'
-              height='1000'
-              className='mt-[100px]'
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              title="Youtube video" allowFullScreen>
-            </iframe>
-          </div>
-        </Container>
-      </div>
-    </section>
-  )
+export default function MapPage() {
+  return <MapClient />;
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-  }
-  
-  module.exports = nextConfig
-  
+  output: 'export', // static export mode
+  images: {
+    unoptimized: true,  // disable Next.js Image Optimization API
+  },
+}
+
+module.exports = nextConfig
